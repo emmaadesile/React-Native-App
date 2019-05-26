0604628fb168512e433091e53a6264ef2fbb99c2
Emmanuel Adesile <emma2adesile@gmail.com>
2019-05-26 18:38:11 +0100

    homepage: design homepage of the app

---
+++ App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Container>
+        <TitleBar>
+          <Avatar source={require('./assets/avatar.jpg')} />
+          <Title>Welcome Back,</Title>
+          <Name>Emmanuel</Name>
+        </TitleBar>
+        <Subtitle>Continue Learning</Subtitle>
+      </Container>
+    );
+  }
+}
+
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
+`;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
+`;
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
